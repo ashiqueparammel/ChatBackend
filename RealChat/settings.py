932428a2830a34f103151c61b07a446c4a8eee23
ashiqueparammel <ashiqueparammel558@gmail.com>
2024-02-13 15:05:16 +0530

    updated database

--- RealChat/settings.py
+++ RealChat/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 from decouple import config
+from django.conf import settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
