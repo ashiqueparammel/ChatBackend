2d33a76a927c81d27e66fe212338ebc50e1cb944
ashiqueparammel <ashiqueparammel558@gmail.com>
2024-02-16 21:01:12 +0530

    updated users

--- RealChat/settings.py
+++ RealChat/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from datetime import timedelta
+import os
 from pathlib import Path
 from decouple import config
 from django.conf import settings
@@ -208,6 +209,10 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = 'RealChat/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- users/admin.py
+++ users/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+# from django.contrib.auth.admin import UserAdmin
 
 from .models import User
-# Register your models here.
-class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'phone_number', 'is_google')
-    list_filter = ('is_google',  'is_staff', 'is_superuser')
-    search_fields = ('username', 'email', 'phone_number')
+# # Register your models here.
+# class CustomUserAdmin(UserAdmin):
+#     list_display = ('username', 'email', 'phone_number', 'is_google')
+#     list_filter = ('is_google',  'is_staff', 'is_superuser')
+#     search_fields = ('username', 'email', 'phone_number')
 
 
 
-admin.site.register(User, CustomUserAdmin)
+admin.site.register(User)
+# admin.site.register(User, CustomUserAdmin)
